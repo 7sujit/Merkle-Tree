f85a27c3c23c20804f1f8104c86853c86cfd58eb
7sujit <muduli_sk@yahoo.com>
2016-07-03 17:35:27 +0530

    removed unwanted functions

--- src/merkletree/NodeBuilder.java
+++ src/merkletree/NodeBuilder.java
@@ -16,39 +16,37 @@ class NodeBuilder {
      * @param args the command line arguments
      */
     
-
-    
     public static void main(String[] args) throws Exception {
-        
+
         List<MerkleNode> nodeList = new ArrayList<>();
         File folder = new File("/home/sujit/SE-Project/Sample1");
         File[] fileList = folder.listFiles();
         StringBuilder sb = new StringBuilder();
-        
+
         for (File file : fileList) {
             byte[] x = SHA256.getHashFile(file);
             for (byte b : x) {
                 sb.append(String.format("%02x", b));
             }
-            
+
             nodeList.add(new MerkleNode(x, sb.toString()));
             sb.setLength(0);
         }
-        
+
         if(nodeList.size() % 2 == 1) {
             // if odd length
             MerkleNode temp = nodeList.get(nodeList.size() - 1);
             nodeList.add(temp);
         }
-               
+
         System.out.println("size of list : " + nodeList.size());
-        
+
 //        for (MerkleNode node : nodeList) {
 //            System.out.println(node.hexString);
 //        }
-        
+
         // Call the treebuilder to built the tree pass the node list as param.
         TreeBuilder tree = new TreeBuilder(nodeList);
     }
-    
+
 }
--- src/merkletree/TreeBuilder.java
+++ src/merkletree/TreeBuilder.java
@@ -14,17 +14,17 @@ import java.util.List;
  * @author sujit
  */
 public final class TreeBuilder {
-    
+
     MerkleNode[] treeList;
-    
+
     public MerkleNode getTreeRoot() {
         return treeList[0];
     }
-    
+
     public void printTreeRoot() {
         System.out.println(treeList[0].hexString);
     }
-    
+
     public void copyLastN(List<MerkleNode> nodeList) {
         int size = nodeList.size();
         int k = treeList.length - 1;
@@ -32,29 +32,25 @@ public final class TreeBuilder {
             treeList[k] = nodeList.get(i);
         }
     }
-    
-    public MerkleNode calcTreeList(int left, int right) throws NoSuchAlgorithmException {
-        
-        return null;
-    }
-    
+
+      
     public static int leftChild(int idx) {
-        
+
         return 2 * idx + 1;
     }
     public static int rightChild(int idx) {
-        
+
         return 2 * idx + 2;
     }
-    
+
     public static byte[] combine(byte[] a, byte[] b) {
         byte[] c = new byte[a.length + b.length];
         System.arraycopy(a, 0, c, 0, a.length);
         System.arraycopy(b, 0, c, a.length, b.length);
-                
+
         return c;
     }
-    
+
     /**
      *
      * @param idx
@@ -62,42 +58,42 @@ public final class TreeBuilder {
      * @throws java.security.NoSuchAlgorithmException
      */
     public byte[] getChildVal(int idx) throws NoSuchAlgorithmException {
-        
+
         if(treeList[idx] == null) { // recursively compute this
-            
+
             byte[] lhashVal = getChildVal(leftChild(idx));
             byte[] rhashVal = getChildVal(rightChild(idx));
             byte[] concat = combine(lhashVal, rhashVal);
-            
+
             StringBuilder sb = new StringBuilder();
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(concat);
-            
+
             byte[] newHashVal = md.digest();
             for (byte b : newHashVal) {
                 sb.append(String.format("%02x", b));
             }
-            
+
             treeList[idx] = new MerkleNode(newHashVal, sb.toString());
             return treeList[idx].hashValue;
         }
-        
-        
+
+
         return treeList[idx].hashValue;
     }
-    
-    
+
+
     public TreeBuilder(List<MerkleNode> nodeList) throws NoSuchAlgorithmException {
-        
+
         StringBuilder sb = new StringBuilder();
         treeList = new MerkleNode[(nodeList.size() * 2)- 1];
         copyLastN(nodeList);
-        
-        
+
+
         byte[] lhashVal = getChildVal(leftChild(0));
         byte[] rhashVal = getChildVal(rightChild(0));
         byte[] concat = combine(lhashVal, rhashVal);
-        
+
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.update(concat);
 
@@ -105,16 +101,16 @@ public final class TreeBuilder {
         for (byte b : newHashVal) {
             sb.append(String.format("%02x", b));
         }
-        
+
         treeList[0] = new MerkleNode(newHashVal, sb.toString());
-        
+
         System.out.println("Tree size : " + treeList.length);
-        
+
         for (MerkleNode merkleNode : treeList) {
             if(merkleNode != null) {
                 System.out.println(merkleNode.hexString);
             }
         }
-        
+
     }
 }
